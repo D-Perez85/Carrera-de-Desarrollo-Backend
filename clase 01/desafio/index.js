425284b54aec5fcab3f51303e05b6f561f6a7e35
D-Perez85 <damianperez22@hotmail.com>
2024-07-11 21:09:36 -0300

    Ejercicio Integrador de Practica

---
+++ clase 01/desafio/index.js
@@ -0,0 +1,50 @@
+/** Definir variables variables que almacenen los siguiente datos:
+     - Un nombre: “pepe” 
+     - Una edad: 25
+     - Un precio: $99.90
+     - Los nombres de mis series favoritas: “Dark”, “Mr Robot”, “Castlevania”
+     - Mis películas favoritas, en donde cada película detalla su nombre, el año de estreno, y una lista con los nombres de sus protagonistas.
+     - Mostrar todos esos valores por consola
+     - Incrementar la edad en 1 y volver a mostrarla
+     - Agregar una serie a la lista y volver a mostrarla 
+ */
+
+let nombre = "pepe";
+let edad = 25;
+let precio = "$99.90";
+const misSeriesFavoritas = ["Dark", "Mr Robot", "Castelvania"];
+let misPeliculasFavoritas = [
+  {
+    titulo: "León, el Profesional",
+    estreno: 1994,
+    protagonistas: ["Jean Reno", "Natalie Portman"],
+  },
+  {
+    titulo: "El Señor de los Anillos: La Comunidad del Anillo",
+    estreno: 2001,
+    protagonistas: ["Elijah Wood", "Ian McKellen"],
+  },
+  {
+    titulo: "El Señor de los Anillos: Las dos torres",
+    estreno: 2002,
+    protagonistas: ["Elijah Wood", "Sean Bean"],
+  },
+  {
+    titulo: "El Señor de los Anillos: El retorno del rey",
+    estreno: 2003,
+    protagonistas: ["Elijah Wood", "Viggo Mortensen"],
+  },
+];
+
+console.log(nombre); // pepe
+console.log(edad); // 25
+console.log(precio); // 99.90
+console.log(misSeriesFavoritas); // lista de series
+console.log(misPeliculasFavoritas); // detalle de peliculas
+
+edad += 1;
+console.log(edad); // 26
+
+misSeriesFavoritas.push("Los favoritos de midas");
+console.log(misSeriesFavoritas); // lista de series modificado
+
